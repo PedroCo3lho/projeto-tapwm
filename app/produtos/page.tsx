2b5cbb68af793edc75e6026414f7427cb7b93931
pedroco3lho <pedrocoelho.nascimento@gmail.com>
2024-09-27 16:37:38 -0300

    estilização e pdf exporting

--- app/produtos/page.tsx
+++ app/produtos/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { useEffect, useState } from 'react';
 import { supabase } from '../../lib/supabase';
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
 
 interface Product {
   id: number;
@@ -20,23 +22,56 @@ const ProductsPage = () => {
     fetchProducts();
   }, []);
 
+  const exportPDF = () => {
+    const doc = new jsPDF();
+
+    doc.text('Lista de Produtos', 20, 10);
+    
+    const tableColumn = ['Nome', 'Quantidade', 'Preço'];
+    const tableRows: (string | number)[][] = [];
+
+    products.forEach((product) => {
+      const productData = [
+        product.nome_produto,
+        product.qtd_produto,
+        `R$ ${product.preco_produto.toFixed(2)}`,
+      ];
+      tableRows.push(productData);
+    });
+
+    // Gerar a tabela no PDF
+    (doc as any).autoTable({
+      head: [tableColumn],
+      body: tableRows,
+      startY: 20,
+    });
+
+    doc.save('produtos.pdf');
+  };
+
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl mb-4">Lista de Produtos</h1>
+      <button
+        onClick={exportPDF}
+        className="mb-4 bg-blue-500 text-white py-2 px-4 rounded"
+      >
+        Exportar para PDF
+      </button>
       <table className="min-w-full bg-white border border-gray-300">
         <thead>
           <tr>
-            <th className="border-b py-2 text-black">Nome</th>
-            <th className="border-b py-2 text-black">Quantidade</th>
-            <th className="border-b py-2 text-black">Preço</th>
+            <th className="border-b border-r py-2 text-left text-black">Nome</th>
+            <th className="border-b border-r py-2 text-left text-black">Quantidade</th>
+            <th className="border-b py-2 text-left text-black">Preço</th>
           </tr>
         </thead>
         <tbody>
-          {products.map(product => (
-            <tr key={product.id} className="border-b py-2 text-black text-center">
-              <td>{product.nome_produto}</td>
-              <td>{product.qtd_produto}</td>
-              <td>{product.preco_produto.toFixed(2)}</td>
+          {products.map((product) => (
+            <tr key={product.id} className="border-b text-center">
+              <td className="py-2 border-r text-black">{product.nome_produto}</td>
+              <td className="py-2 border-r text-black">{product.qtd_produto}</td>
+              <td className="py-2 text-black">R$ {product.preco_produto.toFixed(2)}</td>
             </tr>
           ))}
         </tbody>
